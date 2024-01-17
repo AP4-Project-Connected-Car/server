d52c4921c7767bd58ca88e7d2c8fc9c2c047981c
Martin Delebecque <martin-delebecque@outlook.fr>
2024-01-17 14:41:26 +0100

    Use JSON with express

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 /* --------------------------------- Imports -------------------------------- */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Config
 const config = require('./config.json');
@@ -12,11 +13,12 @@ const config = require('./config.json');
 /* ---------------------------- Init HTTP server ---------------------------- */
 
 const app = express();
+app.use(bodyParser.json());
 
 /* ------------------------------- HTTP routes ------------------------------ */
 
 app.get('/ping', (_req, res) => {
-  res.send('Pong !');
+  res.json({message: 'Pong !', httpCode: 200});
 });
 
 /* ---------------------------- Starting servers ---------------------------- */
